7024f6cc8f2ef279d2900baa092855fdb780c49e
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-10-22 20:33:42 -0400

    Initial Commit

---
+++ Capstone_Project_A3/Capstone_Project_A3App.swift
@@ -0,0 +1,20 @@
+//
+//  Capstone_Project_A3App.swift
+//  Capstone_Project_A3
+//
+//  Created by Sasidurka on 2024-10-22.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_Project_A3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
